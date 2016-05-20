a00fe616091a95cb42986eada8232daabf1a8fc1
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-05-20 14:28:00 +0200

    small folder restructure

--- HappyCoding/2016-05-20/Properties/AssemblyInfo.cs
+++ HappyCoding/2016-05-20/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("_2016_05_20_LessonExample")]
+[assembly: AssemblyTitle("_2016_05_20")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("_2016_05_20_LessonExample")]
+[assembly: AssemblyProduct("_2016_05_20")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("66793775-7fb6-4258-8272-0b7ce5c99c94")]
+[assembly: Guid("6418cade-9960-4fe8-a880-7454356b4d4e")]
 
 // Version information for an assembly consists of the following four values:
 //
