eadcd144d6f0cdff03002ce7e1321c010f2dcccc
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-01 11:16:27 +0200

    small refactor

--- HappyCoding/2016-06-01_canvas2/scripts/drawing.js
+++ HappyCoding/2016-06-01_canvas2/scripts/drawing.js
@@ -2,6 +2,30 @@
     console.log("hello this is a test!");
     draw();
 });
+function draw1() {
+    var painting = document.getElementById('mycanvas');
+    var context = painting.getContext('2d');
+    context.strokeStyle = "#FF0000";
+    context.strokeRect(20, 20, 150, 100);
+
+
+    var gradient = context.createLinearGradient(0, 0, 170, 0);
+    gradient.addColorStop("0", "magenta");
+    gradient.addColorStop("0.5", "blue");
+    gradient.addColorStop("1.0", "red");
+
+    // Fill with gradient
+    context.strokeStyle = gradient;
+    context.lineWidth = 5;
+    context.strokeRect(20, 20, 150, 100);
+
+
+
+    context.font = "30px Verdana";
+    // Fill with gradient
+    context.strokeStyle = gradient;
+    context.strokeText("my name is ivan!", 10, 50);
+}
 
 function draw() {
     var painting = document.getElementById('mycanvas');
@@ -27,3 +51,4 @@ function draw() {
     context.strokeStyle = gradient;
     context.strokeText("my name is ivan!", 10, 50);
 }
+
