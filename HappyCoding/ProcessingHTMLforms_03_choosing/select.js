2ab4c682a229ef284f923a198b72e958e831f0db
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-11 09:43:44 +0200

    js example

---
+++ HappyCoding/ProcessingHTMLforms_03_choosing/select.js
@@ -0,0 +1,8 @@
+﻿function init() {
+    var panel = document.getElementById("panel");
+    var list = document.getElementById("citylist");
+
+    var elem = list.options.selectedIndex;
+    panel.innerHTML += "Selected: " + city + " - Index: " + elem;
+}
+onload = init;
