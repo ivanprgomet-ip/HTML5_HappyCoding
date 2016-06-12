f9b65f692c2b22d0390a7f03629a024173a80780
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-12 23:44:29 +0200

    js example

---
+++ HappyCoding/ProcessingHTMLforms_04_formchanges/change.js
@@ -0,0 +1,30 @@
+﻿/*declaring 3 global variables*/
+var panel, field, saved;
+
+
+function update() {
+    panel.innerHTML = saved + " changed to " + field.value + "<br>";
+    saved = field.value;
+}
+function wipe() {
+    panel.innerHTML = "<br>";
+}
+
+function init() {
+    /*initialize the global values*/
+    panel = document.getElementById("panel");
+    field = document.getElementById("txt");
+    saved = field.value;
+
+    /*plus one local variable*/
+    var form = document.getElementById("lang");
+
+
+    /*attaching event hanlder functions to the form object 
+    and textfield object*/
+    form.onreset = wipe;
+    field.onchange = update;
+    wipe();
+}
+/*Execute a JavaScript immediately after a page has been loaded*/
+onload = init;
