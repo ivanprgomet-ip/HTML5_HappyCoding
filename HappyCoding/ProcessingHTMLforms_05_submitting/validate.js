68371507e708ba4bcd0ff4fd6c58dd4e0a9d86b6
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-13 00:12:47 +0200

    js example

---
+++ HappyCoding/ProcessingHTMLforms_05_submitting/validate.js
@@ -0,0 +1,23 @@
+﻿function validate() {
+    if (this.elements["Name"].value === "") {
+        alert("Please enter your name");
+        return false;
+    }
+
+    if ((this.elements["Email"].value.indexOf("@") === -1) || (this.elements["Email"].value.indexOf(".") === -1))
+    {
+        alert("Please enter a valid email adress");
+        return false;
+    }
+}
+
+function init() {
+    var panel = document.getElementById("panel");
+    panel.innerHTML = "Please enter your name and email adress";
+
+    var form = document.getElementById("contact");
+    /*attaching an event handler function to the form object*/
+    form.onsubmit = validate;
+}
+
+onload = init;
