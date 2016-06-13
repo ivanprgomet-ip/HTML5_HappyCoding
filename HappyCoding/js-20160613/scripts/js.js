818e8ecce34ee007d0cb1e5c0b5591a07d698cdd
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-13 13:38:54 +0200

    added a custom js example for testing purposes

---
+++ HappyCoding/js-20160613/scripts/js.js
@@ -0,0 +1,10 @@
+﻿function add() {
+    var text = document.getElementById("text").value;/*value retrieves whatever is in the textarea form*/
+    document.getElementById("panel").innerHTML = text;
+}
+
+function init() {
+    var btn= document.getElementById("btn");
+    btn.style.backgroundColor = "honeydew";//give btn some color at startup
+}
+window.onload = init;
