ed4c6fb8bff59d18c5910c5630ce2b11ee11629e
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-13 20:03:30 +0200

    worked some on making info go from one page to the other using forms and javascript

---
+++ HappyCoding/js-20160613/scripts/buy.js
@@ -0,0 +1,25 @@
+﻿function addToCart(productid) {
+
+    var productname= document.getElementById(productid).getElementsByTagName("legend")[0].innerHTML;
+    var productquantity = document.getElementById(productid).getElementsByTagName("input")[0].value;
+    var msg = document.getElementById("displayer");
+    msg.innerHTML = productquantity + " piece(s) of " + productname + " added to bag";
+
+    sessionStorage.setItem(productname, productquantity);
+
+    msg.style.color = "forestgreen";
+    msg.style.fontWeight = "bold";
+
+
+    
+}
+
+
+function RetrieveCart() {
+    var allitems = document.getElementById("receipt-info");
+
+
+    for (var i in window.sessionStorage) {
+        allitems.innerHTML += sessionStorage.getItem(i);
+    }
+}
