6ed554dc7039b80806154c102799a735b27d5185
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-13 10:10:52 +0200

    did js example 'swapping backgrounds'

---
+++ HappyCoding/CreatingDynamicEffects/rollover.js
@@ -0,0 +1,36 @@
+﻿var obj;
+
+/*the actual event-handler definitions responding to mouse actions*/
+function over() {
+    obj.style.background = "yellow";
+}
+function down() {
+    obj.style.background = "lime";
+}
+function up() {
+    obj.style.background = "yellow";
+}
+function out() {
+    obj.style.background = "aqua";
+}
+
+function init() {
+    obj = document.getElementById("active");
+
+    /*statements dynamicaly styling the div object so it looks like a button*/
+    obj.style.width = "100px";
+    obj.style.background = "aqua";
+    obj.style.padding = "5px";
+    obj.style.border = "2px solid black";
+    obj.style.textAlign = "center";
+
+    /*some extra*/
+    obj.style.cursor = "pointer";
+    
+    /*statements attaching event-handler functions to the div object*/
+    obj.onmouseover = over;
+    obj.onmousedown = down;
+    obj.onmouseup = up;
+    obj.onmouseout = out;
+}
+onload = init;
