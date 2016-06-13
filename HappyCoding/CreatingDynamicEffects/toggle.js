b38160c38f6df2e78ee7e3c963a9748a3e794b87
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-13 11:00:33 +0200

    added js example toggling visibility

---
+++ HappyCoding/CreatingDynamicEffects/toggle.js
@@ -0,0 +1,24 @@
+﻿var pop;
+
+function showPop() {
+    pop.style.visibility = "visible";
+
+}
+function hidePop() {
+    pop.style.visibility = "hidden";
+}
+
+function init() {
+    pop = document.getElementById("popupLayer");
+    /*hide the pop element at startup of page*/
+    pop.style.visibility = "hidden";
+    
+
+
+    var obj = document.getElementById("contentLayer");
+    /*attach event-handlers to the first div element*/
+    obj.onmouseover = showPop;
+    obj.onmouseout = hidePop;
+
+}
+onload = init;
