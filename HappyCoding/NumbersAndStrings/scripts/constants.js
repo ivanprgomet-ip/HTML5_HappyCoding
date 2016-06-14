7170b5fd226057c822067a501bd2781cf38885c7
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-14 10:11:48 +0200

    more js examples

---
+++ HappyCoding/NumbersAndStrings/scripts/constants.js
@@ -0,0 +1,12 @@
+﻿function init() {
+    var panel = document.getElementById('panel');
+    var rad = 4;
+
+    var area = Math.PI * (rad * rad);
+    var circ = 2 * (Math.PI * rad);
+
+    panel.innerHTML = 'circle radius: ' + rad + ' feet';
+    panel.innerHTML += '<br/>area: ' + area + ' square feet';
+    panel.innerHTML += '<br/>circumference: ' + circ+ ' feet';
+}
+window.onload= init;
---
+++ HappyCoding/NumbersAndStrings/scripts/math.js
@@ -0,0 +1,12 @@
+﻿function initializer() {
+    var panel = document.getElementById('panel');
+
+    var sq = Math.pow(5, 2);
+    var cb = Math.pow(4, 3);
+
+    panel.innerHTML = 'largest positive: ' + Math.max(sq, cb);
+    panel.innerHTML += '<br>smallest positive: ' + Math.min(sq, cb);
+    panel.innerHTML += '<br>smallest negative: ' + Math.max(-5, -4.15);
+
+}
+window.onload = initializer;
