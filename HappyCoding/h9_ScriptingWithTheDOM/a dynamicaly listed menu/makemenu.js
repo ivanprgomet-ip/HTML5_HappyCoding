a681ec116b780607862d757859da111d3c788d51
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-17 23:56:14 +0200

    added js ex of dynamically creating an menu

---
+++ HappyCoding/h9_ScriptingWithTheDOM/a dynamicaly listed menu/makemenu.js
@@ -0,0 +1,45 @@
+﻿function makeMenu() {
+    var h2Headings = document.getElementsByTagName('h2');
+
+    /*create div container to hold the menu, the ul will 
+    be inside the div, which will contain the list items*/
+    var menu = document.createElement('div');
+    var menuUl = document.createElement('ul');
+    menu.appendChild(menuUl);
+
+    //cycle through the collection of h2 headings
+    for (var i = 0; i < h2Headings.length; i++) {
+        //do things for each heading...
+
+        //get text node of h2 element
+        var itemText = h2Headings[i].childNodes[0].nodeValue
+
+        //add a list item
+        var menuLi = document.createElement('li');
+
+        //add the list item to the menu
+        menuUl.appendChild(menuLi);
+
+        //the list item contains a link 
+        var menuLiA = document.createElement('a');
+        menuLiA = menuLi.appendChild(menuLiA);
+
+        //set the href of the link
+        menuLiA.setAttribute('href', '#item' + i)
+
+        //set the text of the link
+        var menuText = document.createTextNode(itemText);
+        menuLiA.appendChild(menuText);
+
+        //create matching anchor element
+        var anc = document.createElement('a');
+        anc.setAttribute('name', 'item' + i);
+
+        //add anchor before the heading
+        document.body.insertBefore(anc, h2Headings[i]);
+    }
+
+    //add menu to the top of the page
+    document.body.insertBefore(menu, document.body.firstChild);
+}
+window.onload = makeMenu;
--- HappyCoding/h9_ScriptingWithTheDOM/replace.js
+++ HappyCoding/h9_ScriptingWithTheDOM/replace.js
@@ -1,1 +1,18 @@
-﻿
+﻿function replaceHeading() {
+
+    var newH2 = document.createElement('h2');
+    var newH2Text = document.createTextNode('Greetings!');
+    newH2.appendChild(newH2Text);
+
+    /*replace unwanted child node of the <div> element with 
+    the new one created above*/
+    var myDiv = document.getElementById('id1');
+    var oldP = document.getElementById('para2');
+    myDiv.replaceChild(newH2, oldP);
+
+
+}
+window.onload = function () {
+    document.getElementById('btn').onclick = replaceHeading;
+}
+
