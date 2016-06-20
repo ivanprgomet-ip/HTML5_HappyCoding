ff5753440a464938611b7005ff330ca6739e5e6d
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-20 13:24:19 +0200

    js example of changing element class using classname

---
+++ HappyCoding/h12_JsAndCSS/classname.js
@@ -0,0 +1,13 @@
+﻿function toggleClass() {
+    var myElement = document.getElementById('id1');
+    if (myElement.className == 'classA') {
+        myElement.className = 'classB';
+    }
+
+    else{
+        myElement.className = 'classA';
+    }
+}
+window.onload = function () {
+    document.getElementById('btn1').onclick = toggleClass;
+}
