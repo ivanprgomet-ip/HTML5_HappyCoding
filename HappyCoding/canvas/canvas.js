5f0285fd4e3de805896a11fa9affbd9929efe984
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-20 11:22:09 +0200

    canvas example

---
+++ HappyCoding/canvas/canvas.js
@@ -0,0 +1,24 @@
+﻿var context;
+var x = 50;
+var y = 50;
+var counter = 0;
+
+function paint() {
+    context.beginPath();
+    context.fillstyle = '#ff0000';
+    context.arc(x, y, 15, 0, Math.PI * 2, false);
+    context.closePath();
+    context.fill();
+}
+function animate() {
+    context.clearRect(0, 0, 400, 300);
+    counter++;
+    x += 20 * Math.sin(counter);
+    y += 20 * Math.cos(counter);
+    paint();
+}
+
+window.onload = function () {
+    context = canvas1.getContenxt('2d');
+    setInterval(animate, 100);
+}
