11b60c679157e6d867bffd7f40793f01b6030dfc
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-20 14:45:04 +0200

    js example selecting stylesheets by title done. for some reasong doesnt work on chrome but works on ie

---
+++ HappyCoding/h12_JsAndCSS/selectstylesheet.js
@@ -0,0 +1,38 @@
+﻿function ssEnable(mySheet) {
+    var change = false;
+    var oldSheet = 0;//currently active stylesheet
+    
+    //cycle through stylesheets array
+    for (var i = 0; i < document.styleSheets.length; i++) {
+        if (document.styleSheets[i].disabled == false) {
+            oldSheet = i;
+        }
+        document.styleSheets[i].disabled = true;
+
+        if(document.styleSheets[i].title==mySheet)
+        {
+            document.styleSheets[i].disabled = false;
+            change = true;
+        }
+    }
+    if (!change)
+    {
+        document.styleSheets[oldSheet].disabled = false;
+    }
+    return change;
+}
+
+function sheet() {
+    //prompt the user for a stylesheet name every time the page loads
+    var sheetName = prompt('Stylesheet name?');
+    if (!ssEnable(sheetName))
+    {
+        alert('not found - original stylesheet retainer.');
+    }
+}
+
+
+window.onload = function () {
+    document.getElementById('btn1').onclick = sheet;
+
+}
---
+++ HappyCoding/h12_JsAndCSS/stylesheet.js
@@ -0,0 +1,13 @@
+﻿var whichsheet = 0;
+document.styleSheets[1].disabled = true;
+
+function sheet() {
+    document.styleSheets[whichsheet].disabled = true;//disable stylesheet whose index is stored in variable whichsheet
+    whichsheet = (whichsheet == 1) ? 0 : 1;//toggle variable whichsheet between 1 and 0
+    document.styleSheets[whichsheet].disabled = false;//enable stylesheet corresoponding to the new value of whichsheet
+    //the combined effect of these acrivities is to toggle between the two active stylesheets for the page
+}
+window.onload = function () {
+    document.getElementById('btn1').onclick = sheet;
+}
+
