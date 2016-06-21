b9d6314b67ea0da77b4ffc723e2b896c8b76666c
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-21 16:34:53 +0200

    some more jquery demonstration examples

---
+++ HappyCoding/UsingLibraries/jqueryanimation.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function(){
+
+    $('#animateMe').text('changing shape...').animate(
+        {
+            width: '400px',
+            height: '200px'
+        }, 5000, function () {
+            //callback function
+            $(this).text('fading away...').fadeOut(4000);
+        }
+    );
+});
