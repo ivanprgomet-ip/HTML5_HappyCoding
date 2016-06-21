c0f6c6ea15fa38f9cc3d6d8a019e63ef4b3e184b
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-21 15:07:28 +0200

    demonstrating the prototype js library and its handling of form values

---
+++ HappyCoding/UsingLibraries/formandjquery.js
@@ -0,0 +1,9 @@
+﻿function showFormFields() {
+    var form = $('exampleForm');
+    var message = '';
+    var fields = form.getElements();
+    for (var x = 0; x < fields.length; x++) {
+        message += 'field name: ' + fields[x].name + ' value: ' + fields[x].value + '\n';
+    }
+    alert(message);
+}
