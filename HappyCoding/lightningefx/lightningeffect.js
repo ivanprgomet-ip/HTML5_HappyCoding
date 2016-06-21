08121ebd736b4cf839d0796c80fa7f726afd5cca
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-06-21 14:45:33 +0200

    js and css lightning effect demonstrated

---
+++ HappyCoding/lightningefx/lightningeffect.js
@@ -0,0 +1,16 @@
+﻿window.onload = function () {
+    document.getElementById('btn1').onclick = function () {
+        document.getElementById('div1').style.background = 'radial-gradient(at top left, white, #6699cc';
+        document.getElementById('div2').style.boxShadow= '10px 10px 10px #808080';
+    }
+    document.getElementById('btn2').onclick = function () {
+        document.getElementById('div1').style.background = 'radial-gradient(at top right, white, #6699cc';
+        document.getElementById('div2').style.boxShadow = '-10px 10px 10px #808080';
+    }
+    document.getElementById('btn3').onclick = function () {
+        document.getElementById('div1').style.background = 'radial-gradient(at bottom, white, #6699cc';
+        document.getElementById('div2').style.boxShadow = '0px -10px 10px #808080';
+    }
+
+
+}
